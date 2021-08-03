3af067ac89fab44c22622b9b11f3cb9bbeb04667
Patrick Zabel <patrick.zabel@exxeta.com>
2021-08-03 14:40:11 +0200

    Lesson Classes and Interfaces finished

---
+++ dist/descriminated-union.js
@@ -0,0 +1,12 @@
+"use strict";
+function moveAnimal(animal) {
+    let speed;
+    switch (animal.type) {
+        case 'bird':
+            speed = animal.flyingSpeed;
+            break;
+        case "horse":
+            speed = animal.runningSpeed;
+    }
+    console.log('Moving at speed: ' + speed);
+}
---
+++ dist/index-properties.js
@@ -0,0 +1,5 @@
+"use strict";
+const errorBag = {
+    email: 'Not a valide email!',
+    username: 'Must start with a capital character!'
+};
